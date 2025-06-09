c986564a4d9b6c7a5d26858174b19a911f49ef13
N-888 <mamambell@gmail.com>
2025-06-09 07:33:32 +0300

    calculator_vvod

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ while True:
         num1 = float(input("Введите первое число: "))
         num2 = float(input("Введите второе число: "))
     except ValueError as e:
-        print(f"Ошибка: {e} Введено не число. Попробуйте снова.")
+        print(f"Ошибка: {e} Введено не число. Внимательно Попробуйте снова.")
         continue
 
